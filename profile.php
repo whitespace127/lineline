4a44a5ccf8d9f29f29e594d47e5deca7f86355d7
whitespace127 <fennxfoci@gmail.com>
2018-03-29 15:06:54 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,16 +4,31 @@ namespace LINE\LINEBot\HTTPClient;
 use LINE\LINEBot\Constant\Meta;
 use LINE\LINEBot\Exception\CurlExecutionException;
 use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\Response;
 
 echo ("test 6");
 
 try {
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
+
+    echo ("test a"); 
+   $channelToken = 'zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=';
+    $channelSecret = '63027787afefca74c046df98f144a3df';
+    $apiEndpointBase = 'LINEBOT_API_ENDPOINT_BASE';
+    echo ("test b"); 
+    $bot = new LINEBot(new CurlHTTPClient($channelToken), [
+        'channelSecret' => $channelSecret,
+        'endpointBase' => $apiEndpointBase, // <= Normally, you can omit this
+    ]);  
+    echo ("test c"); 
+/*    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
 echo ("test a");    
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
 echo ("test b");
-/*$response = $bot->getProfile('fennxfoci');
+    
+    
+    
+$response = $bot->getProfile('fennxfoci');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
