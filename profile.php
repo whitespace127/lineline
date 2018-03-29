c920046a61c1a6dc037c29f263767d79ef6c2ca4
whitespace127 <fennxfoci@gmail.com>
2018-03-29 14:28:29 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,6 +4,7 @@ echo ("test");
 try {
 $httpClient = new \vendor\linecorp\line-bot-sdk\src\LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
 $bot = new \vendor\linecorp\line-bot-sdk\src\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
+echo ("$bot");    
 /*$response = $bot->getProfile('fennxfoci');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
