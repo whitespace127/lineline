5b6f9b69d257905c7f2882b6fe00af42525e6e23
Goffee0 <37899455+Goffee0@users.noreply.github.com>
2018-03-29 17:32:03 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -13,7 +13,7 @@ try {
     
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
-$response = $bot->getProfile('<userId>');
+$response = $bot->getProfile('goffee_');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
