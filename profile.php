d51ab30d40950ac492bb91eff70797095177297b
whitespace127 <fennxfoci@gmail.com>
2018-03-29 14:44:32 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,9 +7,9 @@ use LINE\LINEBot\Response;
 echo ("test 4");
 
 try {
-$httpClient = new \LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
 echo ("test a");    
-$bot = new \LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
 echo ("test b");
 /*$response = $bot->getProfile('fennxfoci');
 if ($response->isSucceeded()) {
