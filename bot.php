97dae39cd0f4663368e7f1d86550a417f96aa22a
Goffee0 <37899455+Goffee0@users.noreply.github.com>
2018-03-30 16:57:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,3 @@
-
-
 <?php
  
 $strAccessToken = "uFbH4IE1NOr3gGSvQaRRT4Cd16wjPHBBTTVx9n3ew7Tiv0cGAKUzzMm1GPzwxUPJ0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS6a7z11YyFBkiIr8f2iYTGlmbFXhGXT6QDpKCMt34wo2AdB04t89/1O/w1cDnyilFU=";
@@ -8,15 +6,12 @@ $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
 $strUrl = "https://api.line.me/v2/bot/message/reply";
-
+ 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($strAccessToken);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
-
-if(strtolower($arrJson['events'][0]['message']['text']) == "id"){
+if($arrJson['events'][0]['message']['text'] == "Id" || $arrJson['events'][0]['message']['text'] == "id"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
