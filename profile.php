69a8bb45bbccac71caa35387735fe8638112efc9
Goffee0 <37899455+Goffee0@users.noreply.github.com>
2018-03-30 08:58:31 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -13,7 +13,7 @@ try {
     
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
-$response = $bot->getProfile('goffee_');
+/*$response = $bot->getProfile('goffee_');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
@@ -21,7 +21,7 @@ if ($response->isSucceeded()) {
     echo $profile['statusMessage'];
 }
     
-
+*/
     echo ("test a"); 
    $channelToken = 'zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=';
     $channelSecret = '63027787afefca74c046df98f144a3df';
