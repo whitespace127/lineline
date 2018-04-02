08dcd8950b566787e8a32d3715e6ebe56404fdb4
Goffee0 <37899455+Goffee0@users.noreply.github.com>
2018-04-02 08:56:17 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -5,8 +5,8 @@ include 'LINE/LINEBot.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('uFbH4IE1NOr3gGSvQaRRT4Cd16wjPHBBTTVx9n3ew7Tiv0cGAKUzzMm1GPzwxUPJ0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS6a7z11YyFBkiIr8f2iYTGlmbFXhGXT6QDpKCMt34wo2AdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63027787afefca74c046df98f144a3df']);
 
-for ($x = 0; $x <= 5; $x++) {
-    $Message = "The number is: $x <br>"; 
+for ($x = 1; $x <= 5; $x++) {
+    $Message = "The number is: $x"; 
    $UId = 'U3188dcd776da96f3810a238670b29c2b';
  
   $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($Message);
