73b6eb3a85466add01d39d1410e6bb32cc1fe71e
Goffee0 <37899455+Goffee0@users.noreply.github.com>
2018-04-11 08:31:32 +0700

    Update test_pushMessage.js

--- node-line-bot-api-develop/test_pushMessage.js
+++ node-line-bot-api-develop/test_pushMessage.js
@@ -3,14 +3,14 @@ const line = require('./index')
 
 // init with auth token
 line.init({
-  accessToken: '{YOUR_ACCESS_TOKEN}',
+  accessToken: 'uFbH4IE1NOr3gGSvQaRRT4Cd16wjPHBBTTVx9n3ew7Tiv0cGAKUzzMm1GPzwxUPJ0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS6a7z11YyFBkiIr8f2iYTGlmbFXhGXT6QDpKCMt34wo2AdB04t89/1O/w1cDnyilFU=',
   // (Optional) for webhook signature validation
-  channelSecret: '{YOUR_CHANNEL_SECRET}'
+  channelSecret: '63027787afefca74c046df98f144a3df'
 })
 
 line.client
   .pushMessage({
-    to: '{YOUR_MID}',
+    to: 'U3188dcd776da96f3810a238670b29c2b',
     messages:[
         {
             "type":"text",
